75cdb9978d3e2bca4403dc4931d4dd52bd685bc2
Stefan Eggers <stefan.eggers@ibsolution.de>
2024-10-09 20:59:52 +0200

    fixed conf

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function config(){
         counterUrl: "http://192.168.1.98/cm?cmnd=status%2010",
         storeFileName: "store.txt",
         hoyemilesstore: "hoyemiles.txt",
-        goeChargerUrl: "http://192.168.1.224",
+        goeChargerUrl: "http://192.168.1.222",
         lastset: "lastset.txt",
         activator: "activator",
         minuteReportFile: "minutereport.csv",
