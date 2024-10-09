8f4160eebecd88f4254ed8c628a4f868b39fb064
Stefan Eggers <stefan.eggers@ibsolution.de>
2024-10-09 19:44:30 +0200

    null avoidance

--- counterserver.js
+++ counterserver.js
@@ -56,12 +56,12 @@ polka()
         // load hms
         try { h = await dtu.getPowerDTU();} catch(e){ }
         let strings = {
-            StringNorth : r.MPPT2!= null? r.MPPT2 : 0,
-            StringSouth: r.MPPT1!=null? r.MPPT1:0,
+            StringNorth : isNaN(r.MPPT2)? 0: r.MPPT2 ,
+            StringSouth: isNaN(r.MPPT1)? 0: r.MPPT1,
             StringGarage: h.Power.v,
             Total: r.MPPT1 + r.MPPT2 + h.Power.v
         }
-        strings.Total = strings.Total == null? 0: strings.Total;
+        strings.Total = isNaN(strings.Total)? 0: strings.Total;
         // load charger
         var content = io.read(config.lastset);
         content.load = content.charger != null? content.charger : 0;
