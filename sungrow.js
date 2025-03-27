a8ae171fd7329e3ca9ddc6e512c6acc5d2037d53
Stefan Eggers <stefan.eggers@ibsolution.de>
2025-03-27 08:06:38 +0100

    new version

--- sungrow.js
+++ sungrow.js
@@ -18,18 +18,18 @@ function getPowerinternal(){
 
     const promise = new Promise( (resolve, reject) =>{
         let result = {};
-        const wss = new WebSocket("ws://192.168.1.214:8082/ws/home/overview");
+        const wss = new WebSocket("wss://192.168.1.214/ws/home/overview",{rejectUnauthorized: false});
                                    
         wss.on('error', console.error);
         wss.on('open', function open() {
           wss.send( JSON.stringify({lang: "de", token: "", service: "connect"}));
         });
 
-    
+        let token = null;
         wss.on('message', function message(data) {
           
            
-            let token = null;
+          
             let payload = JSON.parse(data); 
 
             if( payload.result_data != null) {
@@ -37,22 +37,51 @@ function getPowerinternal(){
                 {
                     case "connect" :  {
                         token = payload.result_data.token;
-                        wss.send(JSON.stringify({lang: "en", token: token, service: "devicelist", type: "0", is_check_token : "0" }));
+                        
+                        wss.send(JSON.stringify({lang: "de_de", token: token, service: "login", passwd: "pw1111", username : "user" }));
+                        break;
+                    }
+                    case "login" :  {
+                        token = payload.result_data.token;
+                        
+                        wss.send(JSON.stringify({lang: "de_de", token: token, service: "devicelist", type: "0", is_check_token : "0" }));
+                       // wss.send(JSON.stringify({lang: "en", token: token, service: "runtime"}));
+                       
                         break;
                     }  
                     case "devicelist" : {
-                        let devid = payload.result_data.list[0].dev_id;
-                        wss.send( JSON.stringify({lang: "en", token: token , service: "direct", dev_id: ""+devid }));
+                       // token = payload.result_data.token;
+                        let devid = ""+payload.result_data.list[0].dev_id;
+                        let time = Date.now()
+                      //  wss.send( JSON.stringify({lang: "de_de", token: token , service: "statistics"}));
+                           wss.send( JSON.stringify({lang: "de_de", token: token , service: "direct", dev_id: devid, time123456: time }));
                         break;
                     }
                     case "direct": {
+                       // token = payload.result_data.token;
                         payload.result_data.list.forEach(element => {
                             result[element.name] = parseFloat(element.voltage) * parseFloat(element.current);
                         });
+                        wss.send( JSON.stringify({lang: "de_de", token: token , service: "statistics"}));
+                        break;
+                    }
+                    case "real": {
+                        // token = payload.result_data.token;
+                         payload.result_data.list.forEach(element => {
+                             result[element.name] = parseFloat(element.voltage) * parseFloat(element.current);
+                         });
+                         wss.send( JSON.stringify({lang: "de_de", token: token , service: "statistics"}));
+                         break;
+                     }
+                    case "runtime": {
+                       /* payload.result_data.list.forEach(element => {
+                            result[element.name] = parseFloat(element.voltage) * parseFloat(element.current);
+                        });*/
                         wss.send( JSON.stringify({lang: "en", token: token , service: "statistics"}));
                         break;
                     }
                     case "statistics": {
+
                         if(payload.result_data.list[0].today_energy != null)
                         {
                             result["energy"] = parseFloat(payload.result_data.list[0].today_energy);
@@ -82,4 +111,4 @@ function getPowerinternal(){
 
 module.exports = {
     getPower
-}
+} 
