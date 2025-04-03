94173201a2c5b56bd3d9cbfe4bdeab6da436a647
Stefan Eggers <stefan.eggers@ibsolution.de>
2025-04-03 11:00:21 +0200

    some guards

--- sungrow.js
+++ sungrow.js
@@ -28,78 +28,85 @@ function getPowerinternal(){
         let token = null;
         wss.on('message', function message(data) {
           
-           
-          
-            let payload = JSON.parse(data); 
-
-            if( payload.result_data != null) {
-                switch(payload.result_data.service)
-                {
-                    case "connect" :  {
-                        token = payload.result_data.token;
-                        
-                        wss.send(JSON.stringify({lang: "de_de", token: token, service: "login", passwd: "pw1111", username : "user" }));
-                        break;
-                    }
-                    case "login" :  {
-                        token = payload.result_data.token;
-                        
-                        wss.send(JSON.stringify({lang: "de_de", token: token, service: "devicelist", type: "0", is_check_token : "0" }));
-                       // wss.send(JSON.stringify({lang: "en", token: token, service: "runtime"}));
-                       
-                        break;
-                    }  
-                    case "devicelist" : {
-                       // token = payload.result_data.token;
-                        let devid = ""+payload.result_data.list[0].dev_id;
-                        let time = Date.now()
-                      //  wss.send( JSON.stringify({lang: "de_de", token: token , service: "statistics"}));
-                           wss.send( JSON.stringify({lang: "de_de", token: token , service: "direct", dev_id: devid, time123456: time }));
-                        break;
-                    }
-                    case "direct": {
-                       // token = payload.result_data.token;
-                        payload.result_data.list.forEach(element => {
-                            result[element.name] = parseFloat(element.voltage) * parseFloat(element.current);
-                        });
-                        wss.send( JSON.stringify({lang: "de_de", token: token , service: "statistics"}));
-                        break;
-                    }
-                    case "real": {
-                        // token = payload.result_data.token;
-                         payload.result_data.list.forEach(element => {
-                             result[element.name] = parseFloat(element.voltage) * parseFloat(element.current);
-                         });
-                         wss.send( JSON.stringify({lang: "de_de", token: token , service: "statistics"}));
-                         break;
-                     }
-                    case "runtime": {
-                       /* payload.result_data.list.forEach(element => {
-                            result[element.name] = parseFloat(element.voltage) * parseFloat(element.current);
-                        });*/
-                        wss.send( JSON.stringify({lang: "en", token: token , service: "statistics"}));
-                        break;
-                    }
-                    case "statistics": {
-
-                        if(payload.result_data.list[0].today_energy != null)
-                        {
-                            result["energy"] = parseFloat(payload.result_data.list[0].today_energy);
+            try{
+                let payload = JSON.parse(data); 
+                if( payload.result_data != null) {
+                    switch(payload.result_data.service)
+                    {
+                        case "connect" :  {
+                            token = payload.result_data.token;
+                            
+                            wss.send(JSON.stringify({lang: "de_de", token: token, service: "login", passwd: "pw1111", username : "user" }));
+                            break;
                         }
-                        wss.close();
-                        resolve(result);
-                        break;
-                    }
-                    default: {
-                        wss.close();
-                        resolve(result);
+                        case "login" :  {
+                            token = payload.result_data.token;
+                            
+                            wss.send(JSON.stringify({lang: "de_de", token: token, service: "devicelist", type: "0", is_check_token : "0" }));
+                           // wss.send(JSON.stringify({lang: "en", token: token, service: "runtime"}));
+                           
+                            break;
+                        }  
+                        case "devicelist" : {
+                           // token = payload.result_data.token;
+                            let devid = ""+payload.result_data.list[0].dev_id;
+                            let time = Date.now()
+                          //  wss.send( JSON.stringify({lang: "de_de", token: token , service: "statistics"}));
+                               wss.send( JSON.stringify({lang: "de_de", token: token , service: "direct", dev_id: devid, time123456: time }));
+                            break;
+                        }
+                        case "direct": {
+                           // token = payload.result_data.token;
+                            payload.result_data.list.forEach(element => {
+                                result[element.name] = parseFloat(element.voltage) * parseFloat(element.current);
+                            });
+                            wss.send( JSON.stringify({lang: "de_de", token: token , service: "statistics"}));
+                            break;
+                        }
+                        case "real": {
+                            // token = payload.result_data.token;
+                             payload.result_data.list.forEach(element => {
+                                 result[element.name] = parseFloat(element.voltage) * parseFloat(element.current);
+                             });
+                             wss.send( JSON.stringify({lang: "de_de", token: token , service: "statistics"}));
+                             break;
+                         }
+                        case "runtime": {
+                           /* payload.result_data.list.forEach(element => {
+                                result[element.name] = parseFloat(element.voltage) * parseFloat(element.current);
+                            });*/
+                            wss.send( JSON.stringify({lang: "en", token: token , service: "statistics"}));
+                            break;
+                        }
+                        case "statistics": {
+    
+                            if(payload.result_data.list[0].today_energy != null)
+                            {
+                                result["energy"] = parseFloat(payload.result_data.list[0].today_energy);
+                            }
+                            wss.close();
+                            resolve(result);
+                            break;
+                        }
+                        default: {
+                            wss.close();
+                            resolve(result);
+                        }
                     }
                 }
+                else {
+                    wss.close();
+                    resolve(result);
+                }
             }
-            else {
+            catch(error){
                 wss.close();
                 resolve(result);
             }
+          
+          
+
+            
             
         });
         
