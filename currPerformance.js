a67a810778ecf6a1a42fed4513127a578539f204
Stefan Eggers <stefan.eggers@ibsolution.de>
2025-09-30 10:41:08 +0200

    added dtu safe val

--- currPerformance.js
+++ currPerformance.js
@@ -1,9 +1,24 @@
 
+let currTop = null;
+
+function manageCurrTop(ct){
+
+   
+    let day = new Date().getDay();
+    if(currTop == null || currTop.date != day) { currTop = { date: day, val: ct }; return ct;}
+    if(ct > currTop.val) currTop = { date: day, val: ct };
+    else return currTop.val;
+    
+    return ct;
+}
+
 
 function getCurrPerf(store, sungrow, dtu, currCount){
 
+    let dtuVal = manageCurrTop(parseInt(dtu.YieldDay.v) / 1000);
+
     let records = store.getRecords();
-    let energy = sungrow.energy + parseInt(dtu.YieldDay.v) / 1000
+    let energy = sungrow.energy + dtuVal
     let cOut = currCount.data.StatusSNS.E320.Total_out
     let cIn = currCount.data.StatusSNS.E320.Total_in;
     let sungrowRaw = sungrow;
