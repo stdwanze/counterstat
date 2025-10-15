dcbea4dfdad2731d86dbb481da1d2ee311c459fa
Stefan Eggers <stefan.eggers@ibsolution.de>
2025-10-15 11:48:37 +0200

    add influx

---
+++ influxapi.js
@@ -0,0 +1,126 @@
+
+import Influx from 'influx'
+import config from './config.js';
+
+let powerPw = config().powerPw;
+
+const influx = new Influx.InfluxDB({
+  host: '192.168.1.55',
+  port: 8086,                 // Standardport
+  database: 'powerdata',    // deine Datenbank
+  username: 'loggerPwr',           // optional
+  password: powerPw,       // optional
+})
+
+export function writeGridEnergy(totalConsumption,ownConsumption,delivered,autarchy, time) {
+    influx.getDatabaseNames()
+    .then(names => {
+        if (!names.includes('powerdata')) {
+        return influx.createDatabase('powerdata')
+        }
+    })
+    .then(() => {
+        console.log('Verbindung hergestellt & Datenbank bereit.')
+
+
+        console.log(`Schreibe Datenpunkt: Wert=${val}, Zeit=${time.toISOString()}`);
+        // Beispiel: Datenpunkt schreiben
+        return influx.writePoints([
+        {
+            measurement: 'GridEnergy',
+            tags: { source: 'tasmota' },
+            fields: { totalConsumption: totalConsumption, ownConsumption: ownConsumption, delivered: delivered, autarchy: autarchy },
+            timestamp: time
+        }
+        ])
+    })
+    .catch(err => {
+        
+        console.error('Fehler:', err)
+    })
+
+}
+export function writePVEnergy(sungrow,garage, time) {
+    influx.getDatabaseNames()
+    .then(names => {
+        if (!names.includes('powerdata')) {
+        return influx.createDatabase('powerdata')
+        }
+    })
+    .then(() => {
+        console.log('Verbindung hergestellt & Datenbank bereit.')
+
+
+        console.log(`Schreibe Datenpunkt: Wert=${val}, Zeit=${time.toISOString()}`);
+        // Beispiel: Datenpunkt schreiben
+        return influx.writePoints([
+        {
+            measurement: 'pvEnergy',
+            tags: { source: 'pv' },
+            fields: { sungrow: sungrow,garage: garage },
+            timestamp: time
+        }
+        ])
+    })
+    .catch(err => {
+        
+        console.error('Fehler:', err)
+    })
+
+}
+export function writePV(total,north,south,garage, time) {
+    influx.getDatabaseNames()
+    .then(names => {
+        if (!names.includes('powerdata')) {
+        return influx.createDatabase('powerdata')
+        }
+    })
+    .then(() => {
+        console.log('Verbindung hergestellt & Datenbank bereit.')
+
+
+        console.log(`Schreibe Datenpunkt: Wert=${val}, Zeit=${time.toISOString()}`);
+        // Beispiel: Datenpunkt schreiben
+        return influx.writePoints([
+        {
+            measurement: 'pv',
+            tags: { source: 'pv' },
+            fields: { total: total, north: north, south: south, garage: garage },
+            timestamp: time
+        }
+        ])
+    })
+    .catch(err => {
+        
+        console.error('Fehler:', err)
+    })
+
+}
+export function writeCharger(power,charged, time) {
+    influx.getDatabaseNames()
+    .then(names => {
+        if (!names.includes('powerdata')) {
+        return influx.createDatabase('powerdata')
+        }
+    })
+    .then(() => {
+        console.log('Verbindung hergestellt & Datenbank bereit.')
+
+
+        console.log(`Schreibe Datenpunkt: Wert=${val}, Zeit=${time.toISOString()}`);
+        // Beispiel: Datenpunkt schreiben
+        return influx.writePoints([
+        {
+            measurement: 'chager',
+            tags: { source: 'charger' },
+            fields: { power: power, charged: charged },
+            timestamp: time
+        }
+        ])
+    })
+    .catch(err => {
+        
+        console.error('Fehler:', err)
+    })
+};
+
