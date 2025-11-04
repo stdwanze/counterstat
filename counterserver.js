0479e2e2306995286ec6bd578cf668171f7bb18f
Stefan Eggers <stefan.eggers@ibsolution.de>
2025-11-04 16:44:09 +0100

    added error handler

--- counterserver.js
+++ counterserver.js
@@ -64,59 +64,10 @@ polka()
 
         res.end(JSON.stringify(strings));
     })
-  /*  .get('/portal', async (req, res) => {
-        let h = {}; h.Power = {}; h.Power.v = -1;
-        // load sungrow
-        let r = await getPower();
-        // load hms
-        try { h = await dtu.getPowerDTU(); } catch (e) { }
-        let strings = {
-            StringNorth: isNaN(r.MPPT2) ? 0 : r.MPPT2,
-            StringSouth: isNaN(r.MPPT1) ? 0 : r.MPPT1,
-            StringGarage: h.Power.v,
-            Total: r.MPPT1 + r.MPPT2 + h.Power.v
-        }
-        strings.Total = isNaN(strings.Total) ? 0 : strings.Total;
-        // load charger
-        var content = io.read(config.lastset);
-        content.load = content.charger != null ? content.charger : 0;
-        content.next = content.result != null ? content.result.chargersetting : "off";
-        content.overflow = content.charger != null ? (content.export == true ? content.overflow * -1 : content.overflow) : content.overflow;
-
-        var html = io.readPlain("./public/portal.html").toString();
-
-        function minLengthReturn(s, l) {
-
-            if (s.length < l) {
-                for (i = s.length; i < l; i++) {
-                    s += "-";
-                }
-
-            }
-            return s;
-        }
-
-
-        html = html.replace('{PVSUM}', minLengthReturn(strings.Total.toFixed(0), 3));
-        html = html.replace('{PVNorth}', strings.StringNorth.toFixed(0));
-        html = html.replace('{PVSouth}', strings.StringSouth.toFixed(0));
-        html = html.replace('{PVGarage}', strings.StringGarage.toFixed(0));
-
-        html = html.replace('{Charge}', minLengthReturn(content.load.toFixed(0), 3));
-        html = html.replace('{Next}', content.next);
-
-        html = html.replace('{overflow}', minLengthReturn(content.overflow.toFixed(0), 3));
-
-
-
-
-
-
-        res.end(html);
-
-
+    .get("/error"  , (req, res) => {
+        throw new Error("Dies ist ein absichtlich erzeugter Fehler für Testzwecke.");
     })
-*/
+
     .use((err, req, res, next) => {
         console.error("Fehler beim Verarbeiten der Anfrage:", err);
 
