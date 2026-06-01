bec80773a71105576745bce19f9d17e3b839ee83
Stefan Eggers <stefan.eggers@ibsolution.de>
2026-06-01 15:58:20 +0200

    fixed mess

--- car.js
+++ car.js
@@ -1,49 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const axios = require('axios');
+const TronityConnector = require('./tronity/tronityconnector');
 
 const conf = JSON.parse(fs.readFileSync(path.join(__dirname, 'tronity/conf.json'), 'utf8'));
-const { clientId, clientSecret } = conf.Tronity;
-
-const TOKEN_URL = 'https://app.tronity.io/oauth/authentication';
-const API_BASE = 'https://app.tronity.io/api';
-
-let _token = null;
-let _tokenExpiry = 0;
-let _vehicleId = null;
-
-async function getToken() {
-    if (_token && Date.now() < _tokenExpiry - 60000) return _token;
-    const res = await axios.post(TOKEN_URL, {
-        client_id: clientId,
-        client_secret: clientSecret,
-        grant_type: 'app'
-    }, { timeout: 10000 });
-    _token = res.data.access_token;
-    _tokenExpiry = Date.now() + (res.data.expires_in || 3600) * 1000;
-    return _token;
-}
-
-async function getVehicleId(token) {
-    if (_vehicleId) return _vehicleId;
-    const res = await axios.get(`${API_BASE}/vehicles`, {
-        headers: { Authorization: `Bearer ${token}` },
-        timeout: 10000
-    });
-    const vehicles = res.data.data || res.data;
-    _vehicleId = vehicles[0].id;
-    return _vehicleId;
-}
+const connector = new TronityConnector(conf.Tronity.clientId, conf.Tronity.clientSecret);
 
 async function load() {
     try {
-        const token = await getToken();
-        const vid = await getVehicleId(token);
-        const res = await axios.get(`${API_BASE}/vehicles/${vid}/last_record`, {
-            headers: { Authorization: `Bearer ${token}` },
-            timeout: 10000
-        });
-        return res.data;
+        await connector.authenticate();
+        const vehicles = await connector.getVehicles();
+        if (!vehicles || !vehicles.length) return null;
+        return await connector.getLastRecord(vehicles[0].id);
     } catch (e) {
         console.log('tronity error: ' + e.message);
         return null;
